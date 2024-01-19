404694e9ea7bd4ffc988163940f9e4bbc72cf736
Adhithiyan03 <83748642+Adhithiyan03@users.noreply.github.com>
2024-01-18 20:00:44 -0500

    Add files via upload

--- Phase1/Code/Wrapper.py
+++ Phase1/Code/Wrapper.py
@@ -17,8 +17,6 @@ Worcester Polytechnic Institute
 import numpy as np
 import cv2
 import glob
-import os
-import random
 import scipy
 from skimage.feature import peak_local_max
 import matplotlib.pyplot as plt
@@ -26,19 +24,18 @@ import matplotlib.pyplot as plt
 def corner_detection(image):
     corner = []
     cmap = []
-    for img in image:
-        gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        gray_img = np.float32(gray_img)
-        gray_img= cv2.cornerHarris(gray_img,3,3,0.04)
-        img[gray_img>0.02*gray_img.max()] = [0,0,255]
-        corner.append(img)
-        cmap.append(gray_img)
-    return corner,cmap
+    gray_img = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    harris= cv2.cornerHarris(gray_img,3,3,0.04)
+    image[harris>0.02*harris.max()] = [0,0,255]
+    corner.append(image)
+    return corner,gray_img
 
 def ANMS(img,corner,Nbest):
     c_best = []
-    lm = peak_local_max(corner,min_distance=15)
-    print(lm)#// convert local max values to binary mask
+    corner = np.array(corner)
+    lm = peak_local_max(corner,min_distance=5)
+   #// convert local max values to binary mask
+    print(corner[300,250])
     tot_n = lm.shape[0]
     r = np.zeros(tot_n)
     ed=0
@@ -55,58 +52,39 @@ def ANMS(img,corner,Nbest):
             if corner[x_j,y_j] > corner[x_i,y_i]:
                 ed = (x_j-x_i)**2 + (y_j-y_i)**2
             if ed < r[i]:
-                r[i] =ed
+                r[i] = ed
                 x[i] = x_j
                 y[i] = y_j
+    print(r)
     xbest = np.zeros(Nbest)
     ybest = np.zeros(Nbest)
     indice = r.argsort()
     indice = np.flip(indice)
     for i in range(Nbest):
+
         xbest[i] = x[indice[i]]
         ybest[i] = y[indice[i]]
         cv2.circle(img, (int(xbest[i]), int(ybest[i])), 5, (0, 255, 0), -1)
     c_best = np.concatenate((xbest,ybest))
     return img,c_best
 
-def feature_descriptor(img):
-    np.zeros(41,)
-
-def sum_sq_dist(vec_1, vec_2):
-
-    sum = 0
-    for i in range(len(vec_1)):
-        sum += np.square(vec_1[i] - vec_2[i])
-    return (sum ** 0.5)
-
-def feature_matching(f_vec1, f_vec2, ratio):
-    """
-    Each keypoint/feature vector of size 64x1
-    Attempts to match the points in two vectors if distance small enough
-    Take the ratio of best match to the second best match and if this is below some ratio keep the matched pair or reject it.
-    """
-    matches = []
-    for i in range(len(f_vec1)):
-        dist = []
-        for j in range(len(f_vec2)):
-            dist.append(sum_sq_dist(f_vec1[i], f_vec2[j]))
-        sorted(dist)
-        idx = np.argsort(dist)
-        if dist[idx[0]]/(dist[idx[1]] + 0.001) < ratio:
-            matches.append([f_vec1[i], f_vec2[idx[0]]])
-
-    return matches
-
-def RANSAC(matches, n_max):
-
-    for i in range(n_max):
-        random_pair = random.choice(matches)
-        p1 = random_pair[0]
-        p2 = random_pair[1]
-
-
-
-    
+def feature_descriptor(img, c_best):
+    features = []
+    gray_img =cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    p_size =41
+    x= np.zeros(np.shape(c_best[0]))
+    y = np.zeros(np.shape(c_best[0]))
+    for i in range(len(c_best)):
+        x[i]=int(c_best[i][0])
+        y[i]=int(c_best[i][0])
+        img =np.array(img)
+        f_img = gray_img[int(x[i])-p_size//2:int(x[i])+p_size//2,int(y[i])-p_size//2:int(y[i])+p_size//2]
+        blur_img=cv2.GaussianBlur(f_img,(5,5),0)
+        f_vec = cv2.resize(blur_img, (8,8),interpolation = cv2.INTER_AREA)
+        f_vec = np.reshape(f_vec,(64,1))
+        f_vec=(f_vec-f_vec.mean())/ np.std(f_vec)
+        features.append(f_vec)
+    return features
 
 def main():
     # Add any Command Line arguments here
@@ -115,43 +93,35 @@ def main():
 
     # Args = Parser.parse_args()
     # NumFeatures = Args.NumFeatures
-
+    image=[]
+    gray_image =[]
+    path = glob.glob("C:/Users/DELL/Downloads/YourDirectoryID_p1/YourDirectoryID_p1/Phase1/Data/Train/Set1/*.jpg")
+    for i in path:
+        im = cv2.imread(i)
+        image.append(im)
     """
     Read a set of images for Panorama stitching
     """
-
-    image=[]
-    gray_image =[]
-    # path = glob.glob("C:/Users/DELL/Downloads/YourDirectoryID_p1/YourDirectoryID_p1/Phase1/Data/Train/Set1/*.jpg")
-    # print(path)
-    path = os.path.dirname(os.getcwd()) 
-    filepath = path + "\\Data\\Train\\Set1\\"
-    for i in os.listdir(filepath):
-        print(i)
-        im = cv2.imread(filepath + i)
-        image.append(im)
-
+    anms = []
+    anms_img = []
+    for i,img in enumerate(image):
+        corner,cmap=corner_detection(img)
+        plt.imsave("img"+str(i)+".png",corner)
+        Nbest =10
+        temp_img,temp = ANMS(img,cmap,Nbest)
+        anms_img.append(temp_img)
+        anms.append(temp)
+        plt.imsave("anms_img"+str(i)+".png",temp_img)
+        f_map= feature_descriptor(img,anms)
     """
 	Corner Detection
 	Save Corner detection output as corners.png
 	"""
-    corner, cmap = corner_detection(image)
-    print(np.shape(corner))
-    plt.imsave("img1.png",corner[0])
 
     """
 	Perform ANMS: Adaptive Non-Maximal Suppression
 	Save ANMS output as anms.png
 	"""
-    anms = []
-    anms_img = []
-    Nbest =10
-    for i,img in enumerate(corner):
-        temp_img,temp = ANMS(img, cmap[i],Nbest)
-        anms_img.append(temp_img)
-        anms.append(temp)
-    print(anms)
-    plt.imsave("anms_img.png",anms)
 
     """
 	Feature Descriptors
@@ -162,9 +132,6 @@ def main():
 	Feature Matching
 	Save Feature Matching output as matching.png
 	"""
-    matches = feature_matching(kp1, kp2)
-    matches = sorted(matches, key = lambda x:x.distance)
-    match_img = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
     """
 	Refine: RANSAC, Estimate Homography
